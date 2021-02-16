cd3228460606963c24e491ee12aa811cbbe38169
paruqute <parvathychandran.t@gmail.com>
2021-02-16 14:47:17 +0530

    file name changed

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library</title>
+</head>
+<body>
+    <div class="contaner">
+
+        <div class="row">
+            <div class="col col-12 col-sm-4"></div>
+            <div class="col col-12 col-sm 4">
+
+                <table class="table">
+                    <tr>
+                        <td>Bookname</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Description</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Author</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Publisher</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Distributor</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Price</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button class="btn btn-warning">Submit</button></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="col col-12 col-sm-4"></div>
+        </div>
+    </div>
+</body>
+</html>
